f7ea1c1cc07b7be6370d4d1762e6f21d4d0dc04c
Sebastian Grunditz <sebgr273@student.liu.se>
2021-04-13 09:53:58 +0200

    Feature: new function for creating c-compatible arrays

---
+++ happy_giraffe/arrayMaker.js
@@ -0,0 +1,13 @@
+
+export function createWA(inArray, memBuf) {
+    let wArray;
+    if (inArray[0] % 1 == 0) {
+        wArray = new Int32Array(memBuf, 0, inArray.length);
+    } else {
+        wArray = new Float32Array(memBuf, 0, inArray.length);
+    }
+
+    wArray.set(inArray);
+
+    return wArray;
+}
