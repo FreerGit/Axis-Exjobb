ca6f833841c92c69bfb8047929648df78dddb9d1
Sebastian Grunditz <sebgr273@student.liu.se>
2021-04-13 10:36:47 +0200

    Feature: added more types of arrays

--- happy_giraffe/arrayMaker.js
+++ happy_giraffe/arrayMaker.js
@@ -1,10 +1,42 @@
 
-function createWA(inArray, memBuf) {
+const createWA = (inArray, memBuf, type = "i", offset = 0) => {
     let wArray;
-    if (inArray[0] % 1 == 0) {
-        wArray = new Int32Array(memBuf, 0, inArray.length);
-    } else {
-        wArray = new Float32Array(memBuf, 0, inArray.length);
+    switch (type) {
+        case "i":
+            wArray = new Int32Array(memBuf, offset, inArray.length);
+            break;
+        case "ui":
+            wArray = new UInt32Array(memBuf, offset, inArray.length);
+            break;
+        case "mi":
+            wArray = new Int8Array(memBuf, offset, inArray.length);
+            break;
+        case "mui":
+            wArray = new UInt8Array(memBuf, offset, inArray.length);
+            break;
+        case "si":
+            wArray = new Int16Array(memBuf, offset, inArray.length);
+            break;
+        case "sui":
+            wArray = new UInt16Array(memBuf, offset, inArray.length);
+            break;
+        case "bi":
+            wArray = new Int64Array(memBuf, offset, inArray.length);
+            break;
+        case "bui":
+            wArray = new UInt64Array(memBuf, offset, inArray.length);
+            break;
+        case "f":
+            wArray = new Float32Array(memBuf, offset, inArray.length);
+            break;
+        case "uf":
+            wArray = new UFloat32Array(memBuf, offset, inArray.length);
+            break;
+        case "bf":
+            wArray = new Float64Array(memBuf, offset, inArray.length);
+        case "buf":
+            wArray = new UFloat64Array(memBuf, offset, inArray.length);
+            
     }
 
     wArray.set(inArray);
@@ -12,4 +44,4 @@ function createWA(inArray, memBuf) {
     return wArray;
 }
 
-export { createWA };
+module.exports = { createWA };
