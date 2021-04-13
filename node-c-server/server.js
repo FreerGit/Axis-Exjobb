6ff86e52547228e8979e46b95ea7ed44e0e4692b
FreerGit <fredrik-98@live.se>
2021-04-13 09:45:29 +0200

    feat: share arrays between wasm and js

--- node-c-server/server.js
+++ node-c-server/server.js
@@ -20,28 +20,22 @@ function getRandomArbitrary(min, max) {
 var unsortedList = [...Array(100).keys()].map(i => getRandomArbitrary(0, 20));
 
 const testx = async () => {
-    const imports = { wasi_snapshot_preview1: { proc_exit: () => { } } } // dummy placeholder function, a sacrifice to the emscripten god who demands it
+
     const wasm = await WebAssembly.instantiate(fs.readFileSync(__dirname + "/calc.wasm"), importObject);
     module.exports = wasm.exports;
-    // console.log()
-    // wasiObj.initialize(wasm)
-    wasiObj.start(wasm.instance);
-    console.log(unsortedList)
-    // for (var prop in wasm.instance) {
-    //     console.log(prop.exports)
 
-    // }
+    wasiObj.start(wasm.instance);
+
     console.log(wasm.instance.exports)
+
     const exp = wasm.instance.exports;
     console.log('--------------------')
-    // const myNumber = ccallArrays("testSort", "array", ["array"], { heapIn: "HEAP8", heapOut: "HEAP8", returnArraySize: 100 })
     const array = new Int32Array(exp.memory.buffer, 0, 100)
     array.set(unsortedList);
 
     const myNumber = exp.testSort(array.byteOffset, array.length);
     console.log('--------------------')
 
-    // const myNumber = exp.qsort(unsortedList, unsortedList.length, exp.cmpfunc)
     console.log(myNumber); // 2126
 };
 
