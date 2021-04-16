c10553f1b63d0f2da5765d19822606c50b7858f4
FreerGit <fredrik-98@live.se>
2021-04-16 10:18:26 +0200

    feat: automatic running of benchmarks

--- polybench-benchmarking/run-all-wasm.sh
+++ polybench-benchmarking/run-all-wasm.sh
@@ -8,17 +8,29 @@ do
 done
 
 main () {
+    #note: dont count time if compiling.
     if [ -z "$(ls -A ./wasm-binaries/)" ]; then
         echo "Compiling!!!";
         eval "./compile-wasm.sh"
     fi
+    if [ -z "$(ls -A ./results/)" ]; then
+        for benchmark in $(ls -d -A ./wasm-binaries/*)
+        do
+            filename=$(basename $benchmark .wasm)
+            touch "./results/${filename}.txt"
+        done;
+    fi
     separator="---------------------------------"
+   
 
     for benchmark in $(ls -d -A ./wasm-binaries/*)
     do
         echo $separator
-        echo "running benchmark for ${benchmark:2}"
-        eval "${cmd} ${benchmark}";
+        echo $benchmark
+        filename=$(basename $benchmark .wasm)
+        echo "running benchmark for ${filename}"
+        output=$(eval "${cmd} ${benchmark}");
+        echo "${output}" >> ./results/${filename}.txt
         echo $seperator 
     done;
 }
