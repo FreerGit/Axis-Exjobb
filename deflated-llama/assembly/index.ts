4f608de686593bb5a0dac9f5220c498e49ef948d
Sebastian Grunditz <sebastig@axis.com>
2021-04-28 15:11:23 +0200

    fix: wasmer works with wasi if no function is called

--- deflated-llama/assembly/index.ts
+++ deflated-llama/assembly/index.ts
@@ -4,15 +4,10 @@ import { Console } from 'as-wasi';
 
 export function gcd(a: i32, b: i32): i32 {
   if (b == 0) {
-    Console.log('Gcc is ' + a.toString());
     return a;
   } else {
     return gcd(b, a % b);
   }
 }
 
-Console.log("hej");
-export function xxxx(): void {
-  // gcd(48, 18);
-  Console.log('fdsafdsfds');
-}
+Console.log('gcd(48, 18) = ' + gcd(48, 18).toString());
