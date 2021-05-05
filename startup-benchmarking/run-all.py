e1d1a64837ee09c81dcf972bdc95e971d5f882d7
Sebastian Grunditz <sebastig@axis.com>
2021-05-05 14:51:54 +0200

    feat: change startup docker test, empty C main

--- startup-benchmarking/run-all.py
+++ startup-benchmarking/run-all.py
@@ -2,6 +2,8 @@ import datetime
 import sys
 import subprocess
 import os
+
+# Calls fcall and times it in 8 significant digits
 def timeCall(fcall):
   timerstart = datetime.datetime.now().timestamp() * 1000
   subprocess.call(fcall, shell=True)
@@ -9,74 +11,70 @@ def timeCall(fcall):
   timerobj = timerstop - timerstart
   return timerobj
 
+# Writes the time to filename
 def writeTime(runtime, filename, timeObj):
   with open(f"./results/{runtime}/{filename}.txt","a+") as f:
     f.write(str(timeObj)+'\n')
 
 def main():
-    runargs=sys.argv[1]
-    osarg=sys.argv[2]
+  # Check for correct numbers of args in argv
+  if sys.argc != 2:
+    print(f"Wrong number of arguments provided, 1 needed, {len(sys.argv) provided")
+    exit();
+  #  Either l for linux or m for macos, aarhc not needed for startup
+  osarg=sys.argv[1]
 
-    cmds={
-      "wasmerslow": "wasmer run ",
-      "wasmerllvm": "wasmer run --llvm ",
-      "wasmtime": "wasmtime run ",
-      "runall": "run-all"
-    }
-    cmd = cmds[runargs]
-    passes = 1
+  # Get number of passes to run in while-loop
+  passes = int(input("How many passes? "))
 
-    if cmd == "run-all":
-      passes = int(input("How many passes? "))
+  # If either binary-folder doesn't exist, create it and populate it
+  if not os.path.exists('./wasm-binaries') or not os.path.exists('./c-binaries'):
+    subprocess.call("./compile-all.sh -" + osarg, shell=True)
 
-    ostype={
-        "osx": "m",
-        "linux": "l",
-    }
+  # If either binary-folder is empty, populate it
+  if len(os.listdir('./wasm-binaries')) == 0 or len(os.listdir('./c-binaries')) == 0:
+    subprocess.call("./compile-all.sh -" + osarg, shell=True)
 
-    ost = ostype[osarg]
+  # If the results folder doesn't have all folders, create them
+  if len(os.listdir('./results')) < 2:
+    subprocess.call("mkdir ./results/wasmerslow", shell=True)
+    subprocess.call("mkdir ./results/wasmerfast", shell=True)
+    subprocess.call("mkdir ./results/wasmtime", shell=True)
+    subprocess.call("mkdir ./results/docker", shell=True)
 
-    if not os.path.exists('./wasm-binaries') or not os.path.exists('./c-binaries'):
-        subprocess.call("./compile-all.sh -" + ost, shell=True)
+  cntr = 1
+  seperator="-"*30
+  # Main game-loop
+  while cntr <= passes:
+    cntr += 1
+    for benchmark in os.listdir('./wasm-binaries'):
+      filename=subprocess.check_output(f'basename {benchmark} .wasm', shell=True).decode('UTF-8').rstrip("\n")
+      gtime=f"gtime -f '%M' -ao results/"
 
-    if len(os.listdir('./wasm-binaries')) == 0 or len(os.listdir('./c-binaries')) == 0:
-        rc = subprocess.call("./compile-all.sh -" + ost, shell=True)
-
-    if len(os.listdir('./results')) < 2:
-        subprocess.call("mkdir ./results/c",shell=True )
-        subprocess.call("mkdir ./results/wasmerslow", shell=True)
-        subprocess.call("mkdir ./results/wasmerllvm", shell=True)
-        subprocess.call("mkdir ./results/wasmtime", shell=True)
-        subprocess.call("mkdir ./results/docker ", shell=True)
-
-    cntr = 1
-    seperator="-"*30
-    while cntr <= passes:
-      cntr += 1
-      if cmd == "run-all":
-          for benchmark in os.listdir('./wasm-binaries'):
-            print(seperator)
-            filename=subprocess.check_output(f'basename {benchmark} .wasm', shell=True).decode('UTF-8').rstrip("\n")
-            gtime=f"gtime -f '%M' -ao results/"
-            print(f'running benchmark for {benchmark}')
-
-            calltime = timeCall(f"{gtime}wasmerslow/memory.txt wasmer run ./wasm-binaries/{benchmark}")
-            writeTime("wasmerslow", filename, calltime)
-
-            calltime = timeCall(f"{gtime}wasmerllvm/memory.txt wasmer run --llvm ./wasm-binaries/{benchmark}")
-            writeTime("wasmerllvm", filename, calltime)
-
-            calltime = timeCall(f"{gtime}wasmtime/memory.txt wasmtime run ./wasm-binaries/{benchmark}")
-            writeTime("wasmtime", filename, calltime)
-
-            calltime = timeCall(f"{gtime}c/memory.txt ./c-binaries/{filename}")
-            writeTime("c", filename, calltime)
-
-            calltime = timeCall(f"{gtime}docker/memory.txt docker run --rm --name=benchmark alpine ping -c 1 8.8.8.8")
-            writeTime("docker", filename, calltime)
-      else:
-        pass
       print(seperator)
+      print(f'running benchmark for {benchmark}')
+
+      calltime = timeCall(f"{gtime}wasmerslow/memory.txt wasmer run ./wasm-binaries/{benchmark}")
+      writeTime("wasmerslow", filename, calltime)
+
+      calltime = timeCall(f"{gtime}wasmerllvm/memory.txt wasmer run --llvm ./wasm-binaries/{benchmark}")
+      writeTime("wasmerllvm", filename, calltime)
+
+      calltime = timeCall(f"{gtime}wasmtime/memory.txt wasmtime run ./wasm-binaries/{benchmark}")
+      writeTime("wasmtime", filename, calltime)
+
+      # calltime = timeCall(f"{gtime}c/memory.txt ./c-binaries/{filename}")
+      # writeTime("c", filename, calltime)
+
+      pwd = os.getcwd()
+      copyFile = f"-v {pwd}/c-binaries/{filename}:/exec/{filename}"
+      chmodAndRun = f"chmod +x /exec/{filename}; ./exec/{filename}"
+      calltime = timeCall(f"{gtime}docker/{filename}.txt docker run --rm {copyFile} debian /bin/bash -c \"{chmodAndRun}\"")
+      writeTime("docker", filename, calltime)
+
+      calltime = timeCall(f"{gtime}docker/memory.txt docker run --rm --name=benchmark alpine ping -c 1 8.8.8.8")
+      writeTime("docker", filename, calltime)
+    print(seperator)
 
 if __name__ == "__main__":
   main()
