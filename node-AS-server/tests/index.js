25d38ed9e02fcf4c5f383e812ac83aa261937bae
Fredrik Eriksson <fredrier@axis.com>
2021-05-11 10:54:43 +0200

    feat: craziest websever you have ever seen 4sure

--- node-AS-server/tests/index.js
+++ node-AS-server/tests/index.js
@@ -2,6 +2,8 @@ const assert = require("assert");
 const myModule = require("..");
 const { getString, newString } = require("../lib/helpers")
 
+const express = require('express')
+const app = express();
 
 const doConcatString = (x, y) => {
     const xPtr = newString(x);
@@ -15,10 +17,20 @@ const doFibb = (n) => {
     // let cache = new Map();
     result = myModule.fib(n);
     console.log(result)
+    return result;
 }
 
 assert.strictEqual(myModule.add(1, 2), 3);
 assert.strictEqual(doConcatString("hello", " World"), "hello World")
 
 doFibb(BigInt(1000));
-console.log('OK')
+console.log('OK');
+console.log('Starting Server..');
+
+app.get('/', (req, res) => {
+    res.send(doFibb(BigInt(1000)).toString());
+})
+
+app.listen(3000, () => {
+    console.log('Server has started');
+})
